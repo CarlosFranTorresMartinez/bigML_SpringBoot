472b63ca1e940641c5aad9871aab701ff2247513
CarlosFranTorresMartinez <carlos.torres@vallegrande.edu.pe>
2020-08-01 18:30:24 -0500

    Estandarazisando mi codigo

--- bigml/src/main/java/com/vallegrande/bigml/controller/Demo.java
+++ bigml/src/main/java/com/vallegrande/bigml/controller/Demo.java
@@ -1,66 +1,40 @@
 package com.vallegrande.bigml.controller;
 
 import com.vallegrande.bigml.model.Model;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import com.vallegrande.bigml.services.Conexion;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@CrossOrigin({"*"})
 @RestController
 public class Demo {
-    private Model model = new Model();
+
+    Model model = new Model();
+    private final Conexion conexion = new Conexion();
 
     @PostMapping("/add")
     public String save() throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpPost httpPost = new HttpPost("https://bigml.io/andromeda/prediction?username=bryanbenavente;api_key=6bdd6d1dbf8ac8a270e56896062c3083555bfcc8");
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-            String json = "{\n" +
-                    "    \"model\": \"logisticregression/5f24ad002fb31c2967005efa\",\n" +
-                    "    \"input_data\" : {\n" +
-                    "        \"age\": " + model.getAge() + ",\n" +
-                    "        \"anaemia\": " + model.getAnaemia() + ",\n" +
-                    "        \"creatinine_phosphokinase\": " + model.getCreatinine_phosphokinase() + ",\n" +
-                    "        \"diabetes\": " + model.getDiabetes() + ",\n" +
-                    "        \"ejection_fraction\": " + model.getEjection_fraction() + ",\n" +
-                    "        \"high_blood_pressure\": " + model.getHigh_blood_pressure() + ",\n" +
-                    "        \"platelets\": " + model.getPlatelets() + ",\n" +
-                    "        \"serum_creatinine\": " + model.getSerum_creatinine() + ",\n" +
-                    "        \"serum_sodium\": " + model.getSerum_sodium() + ",\n" +
-                    "        \"sex\": " + model.getSex() + ",\n" +
-                    "        \"smoking\": " + model.getSmoking() + "\n" +
-                    "    }\n" +
-                    "}";
-            StringEntity stringEntity = new StringEntity(json);
-            httpPost.setEntity(stringEntity);
-
-            System.out.println("Request Ejecuta:D " + httpPost.getRequestLine());
-
-            // Create a custom response handler
-            ResponseHandler<String> responseHandler = response -> {
-                int status = response.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = response.getEntity();
-                    System.out.println(":D " + entity);
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } else {
-                    throw new ClientProtocolException("Unexpected response status: " + status);
-                }
-            };
-            String responseBody = httpclient.execute(httpPost, responseHandler);
-            System.out.println("----------------------------------------");
-            System.out.println(responseBody);
-            return responseBody;
-        }
-
+        /*Estructura de Json*/
+        String json = "{\n" +
+                "    \"model\": \"logisticregression/5f24ad002fb31c2967005efa\",\n" +
+                "    \"input_data\" : {\n" +
+                "        \"age\": " + model.getAge() + ",\n" +
+                "        \"anaemia\": " + model.getAnaemia() + ",\n" +
+                "        \"creatinine_phosphokinase\": " + model.getCreatinine_phosphokinase() + ",\n" +
+                "        \"diabetes\": " + model.getDiabetes() + ",\n" +
+                "        \"ejection_fraction\": " + model.getEjection_fraction() + ",\n" +
+                "        \"high_blood_pressure\": " + model.getHigh_blood_pressure() + ",\n" +
+                "        \"platelets\": " + model.getPlatelets() + ",\n" +
+                "        \"serum_creatinine\": " + model.getSerum_creatinine() + ",\n" +
+                "        \"serum_sodium\": " + model.getSerum_sodium() + ",\n" +
+                "        \"sex\": " + model.getSex() + ",\n" +
+                "        \"smoking\": " + model.getSmoking() + "\n" +
+                "    }\n" +
+                "}";
+        /*Retonarmos la respuesta de la conexion*/
+        return conexion.conectar(json);
     }
 }
---
+++ bigml/src/main/java/com/vallegrande/bigml/services/Conexion.java
@@ -0,0 +1,48 @@
+package com.vallegrande.bigml.services;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class Conexion {
+
+    public String conectar(String json) throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            /*Agregamos la api que nos otorga bigML,con nuestro usuario y key*/
+            HttpPost httpPost = new HttpPost("https://bigml.io/andromeda/prediction?username=bryanbenavente;api_key=6bdd6d1dbf8ac8a270e56896062c3083555bfcc8");
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            /*Ingresamos nuestros datos con formato Json*/
+            StringEntity stringEntity = new StringEntity(json);
+            /*Enviamos nuestro Json*/
+            httpPost.setEntity(stringEntity);
+
+            /*Personalisamos los tipos de respuesta*/
+            ResponseHandler<String> responseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Estado de la respuesta " + status);
+                }
+            };
+
+            /*Agregamos  nuestra respuesta*/
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            System.out.println("responseBody" + responseBody);
+
+            /*Obtenes la respuesta del Json*/
+            return responseBody;
+        }
+
+    }
+}
